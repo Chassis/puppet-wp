14284433d3384d7b5dc718bfc4582db56a2c1eff
Daniel Bachhuber <d@danielbachhuber.com>
2013-05-15 12:38:23 -0700

    Don't run the installation commands again if they've already been run once.

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -9,12 +9,14 @@ class wp::cli (
 		exec{ 'git clone wp-cli':
 			command => "/usr/bin/git clone --recursive git://github.com/wp-cli/wp-cli.git $install_path",
 			before => Exec[ 'wp-cli dev-build' ],
+			creates => "$install_path",
 		}
 		# Install composer if needed, run 'composer install', and set up sym link
 		exec { 'wp-cli dev-build':
 			cwd => "$install_path", # wp-cli expects to be installed from pwd
 			command => "/bin/bash $install_path/utils/dev-build",
 			require => Exec[ 'git clone wp-cli' ],
+			creates => '/usr/bin/wp',
 		}
 	}
 
