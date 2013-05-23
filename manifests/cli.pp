82819b572364f269ab6e18dece028f5ed165e02e
Ryan McCue <me@ryanmccue.info>
2013-05-23 22:20:37 +1000

    Switch to using the installer instead of git
    This reduces the dependencies by one, woo!
    
    At the moment, `wp shell` won't work, since boris fails to install without the
    php5-readline and git packages. That's too many dependencies for me.

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -1,22 +1,61 @@
 class wp::cli (
 	$ensure = 'installed',
 	$install_path = '/usr/local/src/wp-cli',
+	$version = 'dev-master'
 ) {
-
 	if 'installed' == $ensure or 'present' == $ensure {
-		# Clone the Git repo
-		exec{ 'git clone wp-cli':
-			command => "/usr/bin/git clone --recursive git://github.com/wp-cli/wp-cli.git $install_path",
-			before => Exec[ 'wp-cli dev-build' ],
-			require => Package[ 'git' ],
-			creates => "$install_path",
+		# Create the install path
+		file { "$install_path":
+			ensure => directory,
 		}
-		# Install composer if needed, run 'composer install', and set up sym link
-		exec { 'wp-cli dev-build':
-			cwd => "$install_path", # wp-cli expects to be installed from pwd
-			command => "/bin/bash $install_path/utils/dev-build",
-			require => [ Exec[ 'git clone wp-cli' ], Package['curl'] ],
-			creates => '/usr/bin/wp',
+
+		# Clone the Git repo
+		exec{ 'wp-cli download':
+			command => "/usr/bin/curl http://wp-cli.org/installer.sh -o $install_path/installer.sh",
+			require => [ Package[ 'curl' ], File[ $install_path ] ],
+			creates => "$install_path/installer.sh",
+		}
+
+		# Ensure we can run the installer
+		file { "$install_path/installer.sh":
+			ensure => "present",
+			mode => "u+x"
+		}
+
+		# Install wp-cli
+		exec { "wp-cli install":
+			command => "$install_path/installer.sh",
+			environment => [
+				"VERSION=$version",
+				"INSTALL_DIR=$install_path"
+			],
+			require => [
+				File[ "$install_path/installer.sh" ],
+				Package[ 'curl' ],
+				Package[ 'php5-cli' ]
+			],
+			creates => "$install_path/wp"
+		}
+
+		# Ensure we can run wp-cli
+		file { "$install_path/wp":
+			ensure => "present",
+			mode => "a+x"
+		}
+
+		# Symlink it across
+		file { '/usr/bin/wp':
+			ensure => link,
+			target => "$install_path/wp",
+			require => File[ "$install_path/wp" ],
+		}
+	}
+	elsif 'absent' == $ensure {
+		file { "/usr/bin/wp":
+			ensure => absent,
+		}
+		file { "/usr/local/src/wp-cli":
+			ensure => absent,
 		}
 	}
 
@@ -24,9 +63,6 @@ class wp::cli (
 		ensure => installed,
 	}
 
-	package { 'git': 
-		ensure => installed,
-	}
 
 	package { 'curl':
 		ensure => installed,
