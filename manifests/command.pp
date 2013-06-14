4aa319a65b642ef3fd8300b3cd0e708f78a812bb
Ryan McCue <me@ryanmccue.info>
2013-06-14 16:43:51 +1000

    is-installed isn't a top-level command

--- manifests/command.pp
+++ manifests/command.pp
@@ -8,6 +8,6 @@ define wp::command (
 		command => "/usr/bin/wp $command",
 		cwd => $location,
 		require => [ Class['wp::cli'] ],
-		onlyif => '/usr/bin/wp is-installed'
+		onlyif => '/usr/bin/wp core is-installed'
 	}
 }
