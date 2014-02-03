ddd0f6dfa6c0b3c771f3dbf9d3e58c75c00f50e5
Bronson Quick <bronson@sennza.com.au>
2014-02-03 13:24:18 +1000

    Run our commands as the www-data user

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -2,7 +2,8 @@ define wp::plugin (
 	$slug = $title,
 	$location,
 	$ensure = enabled,
-	$networkwide = false
+	$networkwide = false,
+	$www_user
 ) {
 	include wp::cli
 
@@ -10,13 +11,13 @@ define wp::plugin (
 		enabled: {
 			$command = "activate $slug"
 
-			exec { "wp install plugin $title":
+			exec { "sudo -u '$www_user' wp install plugin $title":
 				cwd     => $location,
 				command => "/usr/bin/wp plugin install $slug",
 				unless  => "/usr/bin/wp plugin is-installed $slug",
 				before  => Wp::Command["$location plugin $slug $ensure"],
 				require => Class["wp::cli"],
-				onlyif  => "/usr/bin/wp core is-installed"
+				onlyif  => "/usr/bin/sudo -u '$www_user' wp core is-installed"
 			}
 		}
 		disabled: {
