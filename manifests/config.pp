fcd2b9bacf85f33829035c5f3d4186143188dfb2
hdeadman <hal.deadman@gmail.com>
2014-12-28 01:34:52 -0500

    added define for core config and core download

---
+++ manifests/config.pp
@@ -0,0 +1,21 @@
+define wp::config (
+  $location = $title,
+  $dbname    = 'wordpress',
+  $dbuser    = 'wordpress',
+  $dbpass    = 'P@ssw0rd',
+  $dbhost    = 'localhost',
+  $dbprefix  = 'wp_',
+  $dbcharset = 'utf8',
+) {
+  include wp::cli
+
+  $config = "config --path='$location' --dbname='$dbname' --dbuser='$dbuser' --dbpass='$dbpass' --dbhost='$dbhost' --dbprefix='$dbprefix'"
+  
+  exec {"wp config $location":
+    command => "/usr/bin/wp core $config",
+    user => $::wp::user,
+    require => [ Class['wp::cli'] ],
+    path   => "/bin:/usr/bin:/usr/sbin",
+    unless => "ls $location/wp-config.php | grep wp-config.php > /dev/null 2>&1", 
+  }
+}
---
+++ manifests/download.pp
@@ -0,0 +1,30 @@
+define wp::download (
+  $ensure = 'installed',
+  $location = $title,
+  $locale   = 'en_US',
+  $version  = '4.1',
+  $force    = false,
+) {
+  include wp::cli
+
+  if ('installed' == $ensure or 'present' == $ensure) {
+    if ($force) {
+      $download = "download --path='$location' --locale='$locale' --version='$version' --force'"
+    } else {
+      $download = "download --path='$location' --locale='$locale' --version='$version'"
+    }
+    
+	  exec {"wp download $location":
+	    command => "/usr/bin/wp core $download",
+	    user => $::wp::user,
+	    require => [ Class['wp::cli'] ],
+	    path   => "/bin:/usr/bin:/usr/sbin",
+	    unless => "grep wp- $location > /dev/null 2>&1", 
+	  }
+  }
+  elsif 'absent' == $ensure {
+    file { "$location":
+      ensure => absent,
+    }
+  }
+}
