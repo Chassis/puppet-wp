bc4e92607c2b4f8161ce458b3542c61e5dd4edd6
hdeadman <hal.deadman@gmail.com>
2014-12-28 21:38:20 -0500

    changed theme.pp so it can run theme install

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -1,20 +1,33 @@
 define wp::theme (
-	$location,
-	$ensure = enabled
+  $location,
+  $ensure = enabled,
+  $manage_install = false,
+  $install_name = '/tmp/theme.zip', # theme|zip|url 
+  $theme_name = 'theme',
 ) {
-	#$name = $title,
-	include wp::cli
+  #$name = $title,
+  include wp::cli
 
-	case $ensure {
-		enabled: {
-			$command = "activate $title"
-		}
-		default: {
-			fail("Invalid ensure for wp::theme")
-		}
-	}
-	wp::command { "$location theme $command":
-		location => $location,
-		command => "theme $command"
-	}
+  if ($manage_install) {
+    exec {"${location} wp theme install ${theme_name}":
+      command => "/usr/bin/wp theme install ${install_name}",
+      cwd     => $location,
+      user    => $::wp::user,
+      require => [ Class['wp::cli'] ],
+      unless  => "/usr/bin/wp theme is-installed ${theme_name}"
+    }
+  }
+
+  case $ensure {
+    enabled: {
+      $command = "activate $title"
+    }
+    default: {
+      fail("Invalid ensure for wp::theme")
+    }
+  }
+  wp::command { "$location theme $command":
+    location => $location,
+    command => "theme $command"
+  }
 }
