31a75b64df08b85a705f2fcf3a216aeb8d39cbeb
Shady Sharaf <shady@sharaf.me>
2016-07-24 08:45:19 +0200

    Allow installing plugins and activating them once

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -2,17 +2,41 @@ define wp::plugin (
 	$slug = $title,
 	$location,
 	$ensure = enabled,
-	$networkwide = false
+	$activate = true,
+	$networkwide = false,
 ) {
 	include wp::cli
 
+	$network_activate_string = $networkwide ? {
+		true  => '-network',
+		false => '',
+	}
+
+	$network_string = $networkwide ? {
+		true  => ' --network',
+		false => '',
+	}
+
+	$activate_string = $activate ? {
+		true  => join([' --activate', $network_string], ''),
+		false => '',
+	}
+
 	case $ensure {
+		present: {
+			$command = "install $slug$activate_string"
+			$unless  = ["/usr/bin/wp plugin is-installed $slug"]
+		}
+		activated: {
+			$command = "activate $slug$network_string"
+			$onlyif  = ["/usr/bin/wp plugin is-installed $slug"]
+		}
 		enabled: {
 			$command = "activate $slug"
 
 			exec { "wp install plugin $title":
 				cwd     => $location,
-				user    => $::wp::user,				
+				user    => $::wp::user,
 				command => "/usr/bin/wp plugin install $slug",
 				unless  => "/usr/bin/wp plugin is-installed $slug",
 				before  => Wp::Command["$location plugin $slug $ensure"],
@@ -21,21 +45,18 @@ define wp::plugin (
 			}
 		}
 		disabled: {
-			$command = "deactivate $slug"
+			$command = "deactivate $slug$network_string"
+			$onlyif  = ["/usr/bin/wp plugin is-installed $slug"]
 		}
 		default: {
 			fail("Invalid ensure for wp::plugin")
 		}
 	}
 
-	if $networkwide {
-		$args = "plugin $command --network"
-	}
-	else {
-		$args = "plugin $command"
-	}
 	wp::command { "$location plugin $slug $ensure":
 		location => $location,
-		command => $args
+		command  => "plugin $command",
+		onlyif   => $onlyif,
+		unless   => $unless,
 	}
 }
