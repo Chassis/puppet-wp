297f406c97262254d408e94e8a6baa44c2245a8b
Ryan McCue <me@ryanmccue.info>
2016-12-06 14:07:59 +1000

    Convert to network on provision

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,12 +13,27 @@ define wp::site (
 
 	if ( $network == true ) and ( $subdomains == true ) {
 		$install = "multisite-install --subdomains --url='$url'"
+		$convert = "multisite-convert --subdomains --url='$url'"
 	}
 	elsif ( $network == true ) {
 		$install = "multisite-install --url='$url'"
+		$convert = "multisite-convert --url='$url'"
 	}
 	else {
 		$install = "install --url='$url'"
+	}
+
+	if ( $network ) {
+		exec {"wp multisite-convert $location":
+			command => "/usr/bin/wp core $convert",
+			cwd => $location,
+			logoutput => true,
+			user => $::wp::user,
+			require => [ Class['wp::cli'] ],
+			before => [ Exec[ "wp install $location" ] ],
+			onlyif => '/usr/bin/wp core is-installed',
+			unless => '/usr/bin/wp core is-installed --network',
+		}
 	}
 
 	exec {"wp install $location":
