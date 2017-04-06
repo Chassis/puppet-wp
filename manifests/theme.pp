5c74b06bad7a454ce43ab688d5b7897bd89db963
Bronson Quick <bronson@sennza.com.au>
2017-04-06 13:59:42 +1000

    Add better theme support

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -1,20 +1,51 @@
 define wp::theme (
+	$slug = $title,
 	$location,
 	$ensure = enabled
 ) {
-	#$name = $title,
 	include wp::cli
 
 	case $ensure {
 		enabled: {
-			$command = "activate $title"
+			exec { "wp install theme $title --activate$network$held":
+				cwd     => $location,
+				user    => $::wp::user,
+				command => "/usr/bin/wp theme install $slug --activate $held",
+				unless  => "/usr/bin/wp theme is-installed $slug",
+				require => Class["wp::cli"],
+				onlyif  => "/usr/bin/wp core is-installed"
+			}
+		}
+		disabled: {
+			exec { "wp disable theme $title$network$held":
+				cwd     => $location,
+				user    => $::wp::user,
+				command => "/usr/bin/wp theme deactivate $slug",
+				require => Class["wp::cli"],
+				onlyif  => "/usr/bin/wp core is-installed"
+			}
+		}
+		installed: {
+			exec { "wp install theme $title$network$held":
+				cwd     => $location,
+				user    => $::wp::user,
+				command => "/usr/bin/wp theme install $slug --activate $held",
+				unless  => "/usr/bin/wp theme is-installed $slug",
+				require => Class["wp::cli"],
+				onlyif  => "/usr/bin/wp core is-installed"
+			}
+		}
+		deleted: {
+			exec { "wp delete theme $title":
+				cwd     => $location,
+				user    => $::wp::user,
+				command => "/usr/bin/wp theme delete $slug",
+				require => Class["wp::cli"],
+				onlyif  => "/usr/bin/wp core is-installed"
+			}
 		}
 		default: {
-			fail("Invalid ensure for wp::theme")
+			fail( "Invalid ensure argument passed into wp::theme" )
 		}
-	}
-	wp::command { "$location theme $command":
-		location => $location,
-		command => "theme $command"
 	}
 }
