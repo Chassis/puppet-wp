cf7b51942b5b906fe3f9b052e8cd14cef9cd1568
Bronson Quick <bronson@bronsonquick.com.au>
2019-08-26 13:41:12 +1000

    Add site command

---
+++ manifests/site.pp
@@ -0,0 +1,34 @@
+# Create a type for "wp site" commands
+define wp::site (
+  $aliases,
+  $location,
+) {
+  include wp::cli
+
+  create_subsite { $aliases:
+    aliases  => $aliases,
+    location => $location,
+  }
+}
+
+# Create a type for "wp site create"
+define create_subsite (
+  $aliases,
+  $location,
+) {
+  if ( $name != $aliases[0] ) {
+    $slug = regsubst( $name, '.vagrant.local', '')
+  }
+  if ( $slug ) {
+    notice($slug)
+    exec { "wp site create --slug=${slug}":
+      cwd     => $location,
+      user    => $::wp::user,
+      command => "/usr/bin/wp site create --slug=${slug}",
+      unless  => "/usr/bin/wp site list | grep $slug",
+      require => Class["wp::cli"],
+      onlyif  => "/usr/bin/wp core is-installed",
+      logoutput => true
+    }
+  }
+}
