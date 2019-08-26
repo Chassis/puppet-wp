5905a850d506428d771f52432167c8edde821a52
Bronson Quick <bronson@bronsonquick.com.au>
2019-08-26 14:00:38 +1000

    Fix linting in command.pp

--- manifests/command.pp
+++ manifests/command.pp
@@ -1,14 +1,15 @@
+# A class for
 define wp::command (
 	$location,
 	$command
 ) {
 	include wp::cli
 
-	exec {"$location wp $command":
-		command => "/usr/bin/wp $command",
-		cwd => $location,
-		user => $::wp::user,
+	exec {"${location} wp ${command}":
+		command => "/usr/bin/wp ${command}",
+		cwd     => $location,
+		user    => $::wp::user,
 		require => [ Class['wp::cli'] ],
-		onlyif => '/usr/bin/wp core is-installed'
+		onlyif  => '/usr/bin/wp core is-installed'
 	}
 }
