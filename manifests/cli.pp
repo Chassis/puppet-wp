c0e1e455061858d04e228ce5f2f1013c3a391eab
Bronson Quick <bronson@bronsonquick.com.au>
2019-08-26 13:55:16 +1000

    Fix variables

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -7,7 +7,7 @@ class wp::cli (
 
 	if 'installed' == $ensure or 'present' == $ensure {
 		# Create the install path
-		file { [ "$install_path", "$install_path/bin" ]:
+		file { [ "${install_path}", "${install_path}/bin" ]:
 			ensure => directory,
 		}
 
@@ -15,11 +15,11 @@ class wp::cli (
 		exec{ 'wp-cli download':
 			command => "/usr/bin/curl -o $install_path/bin/wp -L https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar",
 			require => [ Package[ 'curl' ], File[ $install_path ] ],
-			creates => "$install_path/bin/wp"
+			creates => "${install_path}/bin/wp"
 		}
 
 		# Ensure we can run wp-cli
-		file { "$install_path/bin/wp":
+		file { "${install_path}/bin/wp":
 			ensure  => 'present',
 			mode    => 'a+x',
 			require => Exec[ 'wp-cli download' ]
@@ -28,8 +28,8 @@ class wp::cli (
 		# Symlink it across
 		file { '/usr/bin/wp':
 			ensure  => link,
-			target  => "$install_path/bin/wp",
-			require => File[ "$install_path/bin/wp" ],
+			target  => "${install_path}/bin/wp",
+			require => File[ "${install_path}/bin/wp" ],
 		}
 	}
 	elsif 'absent' == $ensure {
