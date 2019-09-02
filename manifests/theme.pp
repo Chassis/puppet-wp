306256b7239afb6c4412f60e64bc272ce5a96563
Bronson Quick <bronson@bronsonquick.com.au>
2019-09-02 15:36:32 +1000

    Add new attributes

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -1,13 +1,15 @@
 # A class for WP-CLI theme commands.
 define wp::theme (
 	$location,
-	$ensure = enabled
+	$slug = $title,
+	$ensure = enabled,
+	$onlyif = '/usr/bin/wp core is-installed',
 ) {
 	include wp::cli
 
 	case $ensure {
 		enabled: {
-			$command = "activate ${title}"
+			$command = "activate ${slug}"
 		}
 		default: {
 			fail('Invalid ensure for wp::theme')
@@ -15,6 +17,7 @@ define wp::theme (
 	}
 	wp::command { "${location} theme ${command}":
 		location => $location,
-		command  => "theme ${command}"
+		command  => "theme ${command}",
+		onlyif   => $onlyif,
 	}
 }
