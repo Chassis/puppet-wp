cd1f763186eabbd7c7c32cff128c2884f357ed14
Bronson Quick <bronson@bronsonquick.com.au>
2019-09-03 16:28:19 +1000

    Change to tabs

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,12 +1,12 @@
 # A class for WP-CLI's plugin commands.
 define wp::plugin (
 	$location,
-	$slug = $title,
-	$ensure = enabled,
+	$slug        = $title,
+	$ensure      = enabled,
 	$networkwide = false,
-	$version = 'latest',
-	$unless = undef,
-	$onlyif = '/usr/bin/wp core is-installed',
+	$version     = 'latest',
+	$unless      = undef,
+	$onlyif      = '/usr/bin/wp core is-installed',
 ) {
 	include wp::cli
 
@@ -20,28 +20,28 @@ define wp::plugin (
 
 	case $ensure {
 		activate: {
-      $command = "activate ${slug} ${held}"
-      $unless  = "is-active ${slug}"
+			$command = "activate ${slug} ${held}"
+			$unless = "is-active ${slug}"
 		}
 		enabled: {
-      $command = "plugin install ${slug} --activate ${held}"
-      $unless  = "plugin is-installed ${slug}"
+			$command = "plugin install ${slug} --activate ${held}"
+			$unless = "plugin is-installed ${slug}"
 		}
 		disabled: {
-      $command = "plugin deactivate ${slug}"
+			$command = "plugin deactivate ${slug}"
 		}
 		installed: {
-      $command = "plugin install ${slug} ${held}"
-      $unless  = "plugin is-installed ${slug}"
+			$command = "plugin install ${slug} ${held}"
+			$unless = "plugin is-installed ${slug}"
 		}
 		deleted: {
-      $command = "plugin delete ${slug}"
+			$command = "plugin delete ${slug}"
 		}
 		uninstalled: {
-      $command = "plugin uninstall ${slug} --deactivate"
+			$command = "plugin uninstall ${slug} --deactivate"
 		}
 		default: {
-      fail( 'Invalid ensure argument passed into wp::plugin' )
+			fail('Invalid ensure argument passed into wp::plugin')
 		}
 	}
 	wp::command { "${location} plugin ${command}":
