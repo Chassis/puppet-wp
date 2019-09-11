2690192b03a9e6f0251965cb9278ff293a2c4a69
Bronson Quick <bronson@bronsonquick.com.au>
2019-09-11 12:11:26 +1000

    Fix tabs

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,34 +1,35 @@
 # Create a type for "wp site" commands
 define wp::site (
-  $aliases,
-  $location,
+	$aliases,
+	$location,
 ) {
-  include wp::cli
+	include wp::cli
 
-  create_subsite { $aliases:
-	aliases  => $aliases,
-	location => $location,
-  }
+	create_subsite { $aliases:
+		aliases  => $aliases,
+		location => $location,
+	}
 }
 
 # Create a type for "wp site create"
 define create_subsite (
-  $aliases,
-  $location,
+	$aliases,
+	$location,
 ) {
-  # Generate the slugs for the subsites.
-  if ( $name != $aliases[0] ) {
-	$slug = regsubst( $name, ".${aliases[0]}", '')
-  }
-  if ( $slug ) {
-	exec { "wp site create --slug=${slug}":
-	  cwd       => $location,
-	  user      => $::wp::user,
-	  command   => "/usr/bin/wp site create --slug=${slug}",
-	  unless    => "/usr/bin/wp site list | grep $slug",
-	  require   => Class['wp::cli'],
-	  onlyif    => "/usr/bin/wp core is-installed",
-	  logoutput => true
+	# Generate the slugs for the subsites.
+	if ( $name != $aliases[0] ) {
+		$slug = regsubst( $name, ".${aliases[0]}", '')
 	}
-  }
+
+	if ( $slug ) {
+		exec { "wp site create --slug=${slug}":
+			cwd       => $location,
+			user      => $::wp::user,
+			command   => "/usr/bin/wp site create --slug=${slug}",
+			unless    => "/usr/bin/wp site list | grep $slug",
+			require   => Class['wp::cli'],
+			onlyif    => "/usr/bin/wp core is-installed",
+			logoutput => true
+		}
+     }
 }
